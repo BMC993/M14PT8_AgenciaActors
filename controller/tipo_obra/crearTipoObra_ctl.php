d4b471055838e0e6295d1307fcddbaa1e142d771
Unknown <guillemci96@gmail.com>
2017-03-21 17:50:41 +0100

    Afegir tipo_obra + inci filtres

---
+++ controller/tipo_obra/crearTipoObra_ctl.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    if (isset($_REQUEST['id']) && isset($_REQUEST['nom']) && isset($_REQUEST['cognom']) && isset($_REQUEST['nif'])) {
+        $id = $_REQUEST['id'];
+        $nom = $_REQUEST['nom'];
+        $cognom = $_REQUEST['cognom'];
+        $nif = $_REQUEST['nif'];
+
+        $agencia->afegirDirector(addslashes($nom), addslashes($cognom), addslashes($nif));
+        //$videoclub->afegirPelicula(addslashes($_POST['titol']), addslashes($_POST['director']), addslashes($_POST['productora']));
+    } else {
+        echo 'caca';
+    }
+} else {
+    include 'view/header.php';
+    include 'view/director/formDirector.php';
+    include 'view/footer.php';
+    
+}
