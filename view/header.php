ac3967da378a66f38eb60ecafe81f60bad0eec33
Pocurull <josep.pocurull97@gmail.com>
2017-03-23 16:49:14 +0100

    header

--- view/header.php
+++ view/header.php
@@ -34,7 +34,7 @@
                     <ul>
                        <a href="?ctl=inici"><li class="col-sm-2 col-xs-12">Inicio</li></a>
                        <a href="{{path('frontend_proyectos')}}"><li class="col-sm-2 col-xs-12">Proyectos</li></a>
-                       <a href="?ctl=director&act=crear"><li class="col-sm-2 col-xs-12">Directores</li></a>
+                       <a href="?ctl=director&act=llistar"><li class="col-sm-2 col-xs-12">Directores</li></a>
                        <a href="?ctl=actor&act=llistar"><li class="col-sm-2 col-xs-12">Actores</li></a>
                        <a href="?ctl=tipoObra&act=crear"><li class="col-sm-2 col-xs-12">Tipo Obres</li></a>
                        <a href="?ctl=tipoPaper&act=crear"><li class="col-sm-2 col-xs-12">Tipo Paper</li></a>
@@ -44,4 +44,4 @@
        </div>
         <div id="main" class="container-fluid">
         <div class="row2">
-            <div id="container" class="col-xs-12">
+            <div id="container" class="container">
