c7d8c9e8bef7cc2543773447c1a4f3d417d7edeb
root <joseppocurull@gmail.com>
2017-03-23 16:33:41 +0100

    S'ha borrat el ego del Berbané

--- controller/actor/llistarActor_ctl.php
+++ controller/actor/llistarActor_ctl.php
@@ -1,18 +1,7 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of llistarActor_ctl
- *
- * @author bernabe
- */
 $llistaActors = $agencia->recuperarActors();
 include 'view/header.php';
 include 'view/actor/llistaActor.php';
 include 'view/footer.php';
-
+?>
